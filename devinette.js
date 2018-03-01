a233eb41842235a8abf6029f13008b58c7cb1d5b
guzzler13 <34184155+guzzler13@users.noreply.github.com>
2018-03-01 22:31:31 +0100

    Add files via upload

---
+++ devinette.js
@@ -0,0 +1,22 @@
+var signe = "#";
+
+if((rep == "yes") || (rep == "oui")) {
+
+	for (var i = 0; i < 7; i++) {
+		
+  		console.log(signe);
+  		signe += "#";
+	}
+}
+
+else if((rep == "no") || (rep == "non")) {
+
+	alert("Quel dommmaaaaaage.... :-D");
+	
+}
+
+else {
+
+	alert("Je vous sens comme tiraillé");
+	
+}
