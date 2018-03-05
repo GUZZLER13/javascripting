c54123c39a3e2ee11d19194f5c9e3a6056ed50bf
guzzler13 <34184155+guzzler13@users.noreply.github.com>
2018-03-05 22:47:10 +0100

    rajout plusieurs oui et non (majuscules,etc...)

--- devinette.js
+++ devinette.js
@@ -1,15 +1,14 @@
 var signe = "#";
 
-if((rep === "yes") || (rep === "oui")) {
+if((rep === "yes") || (rep === "oui") || (rep === "Yes") || (rep === "YES") || (rep === "Oui") || (rep === "OUI")) {
 
 	for (var i = 0; i < 7; i++) {
-		
-  		console.log(signe);
+		console.log(signe);
   		signe += "#";
 	}
 }
 
-else if((rep === "no") || (rep === "non")) {
+else if((rep === "no") || (rep === "non") || (rep === "No") || (rep === "NO") || (rep === "Non") || (rep === "NON")) {
 
 	alert("Quel dommmaaaaaage.... :-D");
 	
